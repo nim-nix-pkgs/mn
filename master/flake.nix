71b5d3e2b83c76a20444ede021e13e5cca812862
hugosenari <hugosenari@gmail.com>
2022-03-12 03:29:49 -0300

    chore: re index mn

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A truly minimal concatenative programming language.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mn-master.flake = false;
+  inputs.src-mn-master.owner = "h3rald";
+  inputs.src-mn-master.ref   = "refs/heads/master";
+  inputs.src-mn-master.repo  = "mn";
+  inputs.src-mn-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mn-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mn-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ v0_2_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A truly minimal concatenative programming language.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mn-v0_2_0.flake = false;
+  inputs.src-mn-v0_2_0.owner = "h3rald";
+  inputs.src-mn-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-mn-v0_2_0.repo  = "mn";
+  inputs.src-mn-v0_2_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mn-v0_2_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mn-v0_2_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
